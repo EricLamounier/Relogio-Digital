89d878a8ec3938c0efefa16ef72d5cca899f472c
EricLamounier <eric_lamounier@outlook.com>
2023-01-07 18:49:53 -0300

    first commit

---
+++ scripts/javascript.js
@@ -0,0 +1,81 @@
+function carregar () {
+    var cumprimento = document.querySelector('#cumprimento');
+    var img = document.querySelector('#image');
+    var body = document.querySelector('#body');
+
+
+    var data = new Date();
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
+    var segundo = data.getSeconds();
+
+    var text_hora;
+    var text_minuto;
+    var text_segundo;
+
+    if(hora != 1){
+        text_hora = ' horas';
+    }else{
+        text_hora = ' hora';
+    }
+
+    if(minuto != 1){
+        text_minuto = ' minutos';
+    }else{
+        text_minuto = ' minuto';
+    }
+
+    if(segundo != 1){
+        text_segundo = ' segundos';
+    }else{
+        text_segundo = ' segundo';
+    }
+
+    document.querySelector('#horas').innerText = hora;
+    document.querySelector('#minutos').innerText = minuto;
+    document.querySelector('#segundos').innerText = segundo;
+
+    document.querySelector('#horas_text').innerText = text_hora;
+    document.querySelector('#minutos_text').innerText = text_minuto;
+    document.querySelector('#segundos_text').innerText = text_segundo;
+
+    if(hora >= 0 && hora < 12){
+        img.src = './images/manha.webp';
+        cumprimento.innerHTML = 'Bom dia!'
+        body.style.background = 'url(./images/manha.webp)';
+
+    }else if(hora >= 12 && hora < 18){
+        img.src = './images/tarde.webp';
+        cumprimento.innerHTML = 'Boa tarde!'
+        body.style.background = 'url(./images/tarde.webp)';
+
+    }else{
+        img.src = './images/noite.webp';
+        cumprimento.innerHTML = 'Boa noite!'
+        body.style.background = 'url(./images/noite.webp)';
+    }
+}
+
+setInterval(carregar, 500);
+
+var hour = document.querySelector('#ponteiro-hora');
+var minute = document.querySelector('#ponteiro-minuto');
+var second = document.querySelector('#ponteiro-segundo');
+
+function setRotation(element, rotation){
+    element.style.transform = `rotate(${rotation}deg)`
+}
+
+function setClock(){
+    var date = new Date();
+    
+    setRotation(hour, (360/12)  * date.getHours());
+
+    setRotation(minute, (360/60)  * date.getMinutes());
+
+    setRotation(second, (360/60)  * date.getSeconds());
+    
+}
+
+setInterval(setClock, 500)
+
